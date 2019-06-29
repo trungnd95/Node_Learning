e85f5b03f7d0d3981e2eddc2b0b14c7f684b6499
TrungN <trungnd233@viettel.com.vn>
2019-06-29 18:03:40 +0700

    Init and to lesson 13

---
+++ controllers/storeController.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
+exports.homePage = (req, res) => {
+  res.render("index");
+};
+
+exports.addStore = (req, res) => {
+  res.render("editStore", { title: "Add Store" });
+};
+
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/stores/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  console.log(stores);
+  res.render("stores", { title: "Stores", stores });
+};
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const storeController = require("../controllers/storeController");
+
+// Do work here
+// router.get("/", (req, res) => {
+//   // data = { name: "Trung", age: 23 };
+//   // res.send("Hey! It works!");
+//   // res.json(req.query)
+//   // res.json(req.params);
+//   res.render("hello", {
+//     name: "Trung",
+//     age: 23
+//   });
+// });
+
+const { catchErrors } = require("../handlers/errorHandlers");
+
+router.get("/", storeController.homePage);
+router.get("/stores/add", storeController.addStore);
+router.post("/stores", catchErrors(storeController.createStore));
+router.get("/stores", catchErrors(storeController.getStores));
+module.exports = router;
